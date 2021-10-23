0110edac064e19b20d8dd35175ba0726f2a3de34
Erik Simmler <tgecho@gmail.com>
2021-10-22 21:47:01 -0400

    Remove need to specify timezone (we're all utc in this context) and allow optionally adding a different noticed timestamp

--- lib/src/replay.rs
+++ lib/src/replay.rs
@@ -40,7 +40,7 @@ pub fn replay_feed(
 
 #[cfg(test)]
 mod tests {
-    use chrono::{DateTime, Utc};
+    use chrono::{DateTime, NaiveDateTime, Utc};
     use chronoutil::DateRule;
 
     use crate::{
@@ -49,22 +49,22 @@ mod tests {
     };
 
     fn parse_dt(dt_str: &str) -> DateTime<Utc> {
-        DateTime::parse_from_rfc3339(dt_str)
-            .unwrap()
-            .with_timezone(&Utc)
+        let fmt = "%Y-%m-%dT%H:%M:%S";
+        let ndt = NaiveDateTime::parse_from_str(dt_str, fmt).expect(&format!(
+            "DateTime::parse_from_str(\"{}\", \"{}\") failed with",
+            dt_str, fmt
+        ));
+        DateTime::<Utc>::from_utc(ndt, Utc)
     }
 
-    fn summary_items(items: Vec<(&str, &str)>) -> Vec<FeedSummaryItem> {
+    fn summary_items(items: Vec<(&str, &str, &str)>) -> Vec<FeedSummaryItem> {
         items
             .into_iter()
-            .map(|(id, dt_str)| {
-                let dt = parse_dt(dt_str);
-                FeedSummaryItem {
-                    id: id.to_string(),
-                    title: Some(id.to_string()),
-                    published: dt,
-                    noticed: dt,
-                }
+            .map(|(id, published, noticed)| FeedSummaryItem {
+                id: id.to_string(),
+                title: Some(id.to_string()),
+                published: parse_dt(published),
+                noticed: parse_dt(if noticed == "pub" { published } else { noticed }),
             })
             .collect()
     }
@@ -79,51 +79,44 @@ mod tests {
             .collect()
     }
 
-    fn daily(start: DateTime<Utc>, end: DateTime<Utc>) -> DateRule<DateTime<Utc>> {
-        DateRule::daily(start).with_end(end)
-    }
-
     #[test]
     fn empty_feed() {
         let feed = summary_items(vec![]);
         let result = replay_feed(
             feed,
-            DateRule::daily(parse_dt("2014-11-28T21:00:00+00:00")),
-            parse_dt("2014-12-28T21:00:00+00:00"),
+            DateRule::daily(parse_dt("2014-11-28T21:00:00")),
+            parse_dt("2014-12-28T21:00:00"),
         );
         assert_eq!(result, vec![]);
     }
 
     #[test]
     fn one_item() {
-        let feed = summary_items(vec![("1", "2013-10-10T21:00:00+00:00")]);
+        let feed = summary_items(vec![("1", "2013-10-10T21:00:00", "pub")]);
         let result = replay_feed(
             feed,
-            DateRule::daily(parse_dt("2014-11-28T21:00:00+00:00")),
-            parse_dt("2014-12-28T21:00:00+00:00"),
+            DateRule::daily(parse_dt("2014-11-28T21:00:00")),
+            parse_dt("2014-12-28T21:00:00"),
         );
-        assert_eq!(
-            result,
-            replayed_items(vec![("1", "2014-11-28T21:00:00+00:00")])
-        );
+        assert_eq!(result, replayed_items(vec![("1", "2014-11-28T21:00:00")]));
     }
 
     #[test]
     fn two_items() {
         let feed = summary_items(vec![
-            ("1", "2013-10-10T21:00:00+00:00"),
-            ("2", "2013-11-10T21:00:00+00:00"),
+            ("1", "2013-10-10T21:00:00", "pub"),
+            ("2", "2013-11-10T21:00:00", "pub"),
         ]);
         let result = replay_feed(
             feed,
-            DateRule::daily(parse_dt("2014-11-28T21:00:00+00:00")),
-            parse_dt("2014-12-28T21:00:00+00:00"),
+            DateRule::daily(parse_dt("2014-11-28T21:00:00")),
+            parse_dt("2014-12-28T21:00:00"),
         );
         assert_eq!(
             result,
             replayed_items(vec![
-                ("1", "2014-11-28T21:00:00+00:00"),
-                ("2", "2014-11-29T21:00:00+00:00")
+                ("1", "2014-11-28T21:00:00"),
+                ("2", "2014-11-29T21:00:00")
             ])
         );
     }
@@ -131,39 +124,36 @@ mod tests {
     #[test]
     fn stops_repeating_at_end() {
         let feed = summary_items(vec![
-            ("1", "2013-11-28T21:00:00+00:00"),
-            ("2", "2013-12-28T21:00:00+00:00"),
+            ("1", "2013-11-28T21:00:00", "pub"),
+            ("2", "2013-12-28T21:00:00", "pub"),
         ]);
         let result = replay_feed(
             feed,
-            DateRule::weekly(parse_dt("2014-11-28T21:00:00+00:00")),
-            parse_dt("2014-11-28T22:00:00+00:00"),
+            DateRule::weekly(parse_dt("2014-11-28T21:00:00")),
+            parse_dt("2014-11-28T22:00:00"),
         );
-        assert_eq!(
-            result,
-            replayed_items(vec![("1", "2014-11-28T21:00:00+00:00")])
-        );
+        assert_eq!(result, replayed_items(vec![("1", "2014-11-28T21:00:00")]));
     }
 
     #[test]
     fn resumes_original_schedule_once_caught_up() {
         let feed = summary_items(vec![
-            ("1", "2014-11-01T09:00:00+00:00"),
-            ("2", "2014-11-04T21:00:00+00:00"),
-            ("3", "2014-11-09T22:00:00+00:00"),
-            ("4", "2014-11-13T22:00:00+00:00"),
+            ("1", "2014-11-01T09:00:00", "pub"),
+            ("2", "2014-11-04T21:00:00", "pub"),
+            ("3", "2014-11-09T22:00:00", "pub"),
+            ("4", "2014-11-13T22:00:00", "pub"),
         ]);
         let result = replay_feed(
             feed,
-            DateRule::daily(parse_dt("2014-11-03T20:00:00+00:00")),
-            parse_dt("2014-11-12T22:00:00+00:00"),
+            DateRule::daily(parse_dt("2014-11-03T20:00:00")),
+            parse_dt("2014-11-12T22:00:00"),
         );
         assert_eq!(
             result,
             replayed_items(vec![
-                ("1", "2014-11-03T20:00:00+00:00"),
-                ("2", "2014-11-04T21:00:00+00:00"),
-                ("3", "2014-11-09T22:00:00+00:00"),
+                ("1", "2014-11-03T20:00:00"),
+                ("2", "2014-11-04T21:00:00"),
+                ("3", "2014-11-09T22:00:00"),
             ])
         );
     }
