5128e12ab105e67027517dc9fbf351806d016597
Erik Simmler <tgecho@gmail.com>
2021-12-25 15:28:44 -0500

    Sped up the boundary crossing code by making it more manual/low level
    I'm still not super happy with this, but it's not actually much less typesafe then the json/serde based version, and it's substantially faster, if slightly more fiddly. I'd work more on it if this was going to be the first of many more in depth integrations, but it's a one off goofy project, so I'm gonna move on.

--- lib_wasm/src/lib.rs
+++ lib_wasm/src/lib.rs
@@ -2,7 +2,7 @@ mod utils;
 
 use chrono::{DateTime, Utc};
 use chronoutil::DateRule;
-use podreplay_lib::{reschedule_feed, FeedSummary};
+use podreplay_lib::{reschedule_feed, FeedSummary, SummaryItem};
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -11,22 +11,47 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+use std::time::{Duration, UNIX_EPOCH};
+
+fn dt_from_unix_epoch(seconds: f64) -> DateTime<Utc> {
+    (UNIX_EPOCH + Duration::from_secs_f64(seconds)).into()
+}
+
 #[wasm_bindgen]
 pub fn reschedule(
-    feed: &JsValue,
-    // rule: DateRule<DateTime<Utc>>,
-    start: &JsValue,        // DateTime<Utc>,
-    now: &JsValue,          // DateTime<Utc>,
-    feed_noticed: &JsValue, // DateTime<Utc>,
-) -> JsValue {
-    let feed: FeedSummary = feed.into_serde().unwrap();
-    let start: DateTime<Utc> = start.into_serde().unwrap();
-    let now: DateTime<Utc> = now.into_serde().unwrap();
-    let feed_noticed: DateTime<Utc> = feed_noticed.into_serde().unwrap();
+    items: Vec<f64>,
+    // rule: &str,
+    start: f64,
+    now: f64,
+    feed_noticed: f64,
+) -> Vec<f64> {
+    let length = items.len();
+    let feed = FeedSummary::from_items(
+        items
+            .iter()
+            .enumerate()
+            .map(|(index, timestamp)| SummaryItem {
+                id: index.to_string(),
+                title: "".to_string(),
+                timestamp: dt_from_unix_epoch(*timestamp),
+            })
+            .collect(),
+    );
+    let start = dt_from_unix_epoch(start);
+    let now = dt_from_unix_epoch(now);
+    let feed_noticed = dt_from_unix_epoch(feed_noticed);
 
+    // TODO: don't hardcode this (we need to add a way to configure this on the /replay endpoint as well)
     let rule = DateRule::weekly(start);
     let items = feed.into_cached_items();
 
     let (rescheduled, _) = reschedule_feed(&items, rule, start, now, feed_noticed);
-    JsValue::from_serde(&rescheduled).unwrap()
+
+    (0..length)
+        .into_iter()
+        .map(|index| {
+            let timestamp = rescheduled.get(&index.to_string());
+            timestamp.map_or(0.0, |ts| ts.timestamp() as f64)
+        })
+        .collect()
 }
--- ui/src/util/reschedule.ts
+++ ui/src/util/reschedule.ts
@@ -1,13 +1,29 @@
 import type { FeedSummary } from './fetchFeedSummary';
+import init, * as wasm from 'podreplay_lib_wasm';
+import { fromUnixTime, getUnixTime } from 'date-fns';
 
-export function reschedule(feed: FeedSummary) {
-  return import('podreplay_lib_wasm').then(async (m) => {
-    await m.default();
-    return m.reschedule(
-      feed,
-      '2020-10-03T14:00:00Z',
-      '2021-10-03T14:00:00Z',
-      '2020-10-03T14:00:00Z',
-    );
-  });
+export async function reschedule(feed: FeedSummary): Promise<(null | Date)[]> {
+  await init();
+
+  const items = new Float64Array(feed.items.length);
+  for (let i = 0; i < feed.items.length; i++) {
+    items[i] = getUnixTime(new Date(feed.items[i].timestamp));
+  }
+
+  const timestamps = await wasm.reschedule(
+    items,
+    getUnixTime(new Date('2021-05-03T14:00:00Z')),
+    getUnixTime(new Date()),
+    getUnixTime(new Date('2020-10-03T14:00:00Z')),
+  );
+
+  const results = [];
+  for (const timestamp of timestamps) {
+    if (timestamp !== 0) {
+      results.push(fromUnixTime(timestamp));
+    } else {
+      results.push(null);
+    }
+  }
+  return results;
 }
