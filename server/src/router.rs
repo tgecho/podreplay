3da797146233cda7c231625c526e413016b0337c
Erik Simmler <tgecho@gmail.com>
2022-01-01 14:31:32 -0500

    Add initial static support to rust server

--- server/src/router.rs
+++ server/src/router.rs
@@ -2,11 +2,21 @@ use crate::db::Db;
 use crate::fetch::HttpClient;
 use crate::replay;
 use crate::summary;
+use axum::routing::get_service;
 use axum::{routing::get, AddExtensionLayer, Router};
-use tower_http::trace::TraceLayer;
+use hyper::StatusCode;
+use tower_http::{services::ServeDir, trace::TraceLayer};
 
 pub fn make_router(db: Db, http: HttpClient) -> Router {
     Router::new()
+        .fallback(get_service(ServeDir::new("ui/build")).handle_error(
+            |error: std::io::Error| async move {
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Unhandled internal error: {}", error),
+                )
+            },
+        ))
         .route("/summary", get(summary::get))
         .route("/replay", get(replay::get))
         .layer(AddExtensionLayer::new(db))
