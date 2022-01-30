b828c52f9a993659d96c4bce1ca327601d3c0389
Erik Simmler <tgecho@gmail.com>
2022-01-29 23:14:57 -0500

    Added more attribution

--- ui/src/components/Footer.svelte
+++ ui/src/components/Footer.svelte
@@ -1,7 +1,12 @@
 <footer>
   <p>
-    PodReplay is a coffee table project by <a href="https://esimmler.com">Erik Simmler</a>. Icons by
-    <a href="https://ikonate.com/">Iconate</a>.
+    PodReplay is a coffee table project by <a href="https://esimmler.com">Erik Simmler</a>.
+  </p>
+  <p>
+    Powered by <a href="https://www.rust-lang.org/">Rust</a>,
+    <a href="https://svelte.dev/">Svelte</a>, <a href="https://litestream.io/">Litestream</a>,
+    <a href="https://fly.io/">Fly.io</a>,
+    <a href="https://ikonate.com/">Iconate</a>, and more.
   </p>
   <p>
     While this app is &copy; 2021 Erik Simmler, no claims are made to the contents of any replayed
