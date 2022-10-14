0c45937ddf1979509aef0f5eb5756bce26ee28c2
Erik Simmler <git@getpi.org>
2022-10-14 18:53:13 -0400

    Update UI dependencies
    Test Plan: CI
    
    Reviewers: 
    
    Pull Request: https://github.com/tgecho/podreplay/pull/6

--- ui/src/util/state.ts
+++ ui/src/util/state.ts
@@ -1,7 +1,7 @@
 import { debounce } from 'lodash-es';
 import { page } from '$app/stores';
 import { goto } from '$app/navigation';
-import { browser } from '$app/env';
+import { browser } from '$app/environment';
 
 import { get, writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
---
+++ ui/vite.config.ts
@@ -0,0 +1,20 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { searchForWorkspaceRoot } from 'vite';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [sveltekit()],
+  server: {
+    port: 3000,
+    fs: {
+      allow: [searchForWorkspaceRoot(process.cwd()), './target/wasm-pack'],
+    },
+    proxy: {
+      '/replay': 'http://localhost:8080',
+      '/summary': 'http://localhost:8080',
+      '/plsbl.js': 'http://localhost:8080',
+    },
+  },
+};
+
+export default config;
